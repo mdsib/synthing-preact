4f4895535d1d1dad50b6b77496b6c2f33cc49f10
Massimo Siboldi <mdsiboldi@gmail.com>
2018-03-20 13:09:50 -0700

    Remove pesky logs

--- src/ClickOutside/index.js
+++ src/ClickOutside/index.js
@@ -3,12 +3,10 @@ import { h, Component } from 'preact';
 export default class ClickOutside extends Component {
     handleClick = (ev) => {
         if (!this.elRef.contains(ev.target)) {
-            console.log('actioning');
             this.props.action();
         }
     }
     componentDidMount() {
-        console.log('mounting')
         // setTimeout ensures the click event doesn't get triggered while mounting
         window.setTimeout(() => {
             document.addEventListener('click', this.handleClick);
